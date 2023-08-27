f078dd3a3d0f379d0838ce636f08a096e333e3f7
Raushan <roushankumarsingh2018@gmail.com>
2023-08-27 23:42:16 +0530

    jsx limitation

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,7 @@
+
+const Wrapper= props =>{
+    return props.children; //children props holds all the content u are passing between the opening and closing tag of ur custom component ,
+    // this is mt wrapper
+};
+
+export default Wrapper;
